f232bfeda5a93f7379f84f93aeb170e741544402
Shrey K <srkkadia@gmail.com>
2023-10-23 11:58:47 +0530

    OOPS Start: Objects and Classes

---
+++ OOPs/Basics/Objects_Classes.java
@@ -0,0 +1,21 @@
+
+public class Objects_Classes {
+    public static void main(String[] args) {
+        //
+    }
+}
+
+/*
+Q. What is an Object?
+A. An Object is the physical as well as logical entity.
+
+Ex: chair, pen, table, keyboard, bike, etc.
+
+Q. What is a Class?
+A. A Class is a group of similar objects. It is a logical entity.
+
+Ex: if you have a chair, pen, table, whiteboard, blackboard, chalk, markers, etc. then you can say that you have a "Furniture" in the class.
+
+
+
+*/
