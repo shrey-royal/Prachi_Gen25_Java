7bbf6bf9d171cd516d295a5fc37e70e6b5355536
Shrey K <srkkadia@gmail.com>
2023-10-24 11:29:06 +0530

    Object and Classes

--- OOPs/Basics/Objects_Classes.java
+++ OOPs/Basics/Objects_Classes.java
@@ -1,7 +1,58 @@
+import java.util.Scanner;
+
+class Student {
+    private int rollNo;
+    private String name;
+
+    public void scanStudentDetails() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the roll number: ");
+        rollNo = sc.nextInt();
+
+        sc.nextLine();  // to clear the buffer
+        System.out.print("Enter the name: ");
+        name = sc.nextLine();
+
+        sc.close();
+    }
+
+    public void printStudentDetails() {
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Name: " + name);
+    }
+}
 
 public class Objects_Classes {
+    private int a;
+    int b;
+
+    public void scanVars() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        b = sc.nextInt();
+        sc.close();
+    }
+
+    public void printVars() {
+        System.out.println("a = " + a + "\nb = " + b);
+    }
+
+
     public static void main(String[] args) {
-        //
+        // Objects_Classes obj = new Objects_Classes();
+
+        // obj.scanVars();
+        // obj.printVars();
+
+        // System.out.println("b = " + obj.b);
+
+        Student st = new Student();
+
+        st.scanStudentDetails();
+        st.printStudentDetails();
     }
 }
 
@@ -15,6 +66,10 @@ Q. What is a Class?
 A. A Class is a group of similar objects. It is a logical entity.
 
 Ex: if you have a chair, pen, table, whiteboard, blackboard, chalk, markers, etc. then you can say that you have a "Furniture" in the class.
+
+
+How to create an Object?
+    ClassName objectName = new ClassName();
 
 
 
