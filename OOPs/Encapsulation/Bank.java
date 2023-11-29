daeb29f85482a86fccdbd0f1f677b0158218188f
Shrey K <srkkadia@gmail.com>
2023-11-29 11:48:05 +0530

    Tasks for Encapsulation

--- OOPs/Encapsulation/Bank.java
+++ OOPs/Encapsulation/Bank.java
@@ -19,3 +19,58 @@ public class Bank {
         System.out.println(son.getAccountHolder() + ": " + son.getBalance());
     }
 }
+
+/*
+Tasks: Do Any 5
+
+Package:
+
+Scenario 1: Banking System
+Create a package called "bank" that contains classes like "Account," "Transaction," and "Customer" that are related to banking operations. Place these classes in the "bank" package to organize and group them together.
+
+Scenario 2: E-commerce Platform
+Design a package called "ecommerce" that includes classes like "Product," "Cart," and "Order" that are related to online shopping. Place these classes in the "ecommerce" package to encapsulate and manage them as a cohesive unit.
+
+Scenario 3: Hospital Management System
+Develop a package called "hospital" that contains classes like "Doctor," "Patient," and "Appointment" that handle various aspects of managing a hospital. Place these classes in the "hospital" package to provide organization and separation from other modules.
+
+Scenario 4: Game Development
+Create a package called "game" that includes classes like "Player," "Enemy," and "Level" that are used in a game development project. Place these classes in the "game" package to maintain a logical grouping of game-related components.
+
+Scenario 5: Social Media Application
+Design a package called "socialmedia" that contains classes like "User," "Post," and "Comment" that handle functionalities of a social media application. Place these classes in the "socialmedia" package to keep them organized and separate from other modules.
+
+Access Modifiers:
+
+Scenario 1: Banking System
+In a banking system, use the "private" access modifier for sensitive information like customer account balances to restrict direct access to this data.
+
+Scenario 2: Online Store
+In an online store application, use the "public" access modifier for methods that provide services to customers, such as adding items to a shopping cart or processing payments.
+
+Scenario 3: University Management System
+In a university management system, use the "protected" access modifier for class members that need to be accessed by derived classes, such as a "Student" class accessing the "name" attribute in the "Person" class.
+
+Scenario 4: Employee Database
+In an employee database application, use the "default" (package-private) access modifier for methods and attributes that should only be accessible within the same package, such as utility methods or helper classes.
+
+Scenario 5: Travel Booking System
+In a travel booking system, use a combination of access modifiers, such as "private" for sensitive data like customer payment details, "protected" for methods used within specific subclasses, and "public" for API methods accessible by external systems.
+
+Encapsulation:
+
+Scenario 1: Vehicle Manufacturing
+In a vehicle manufacturing system, encapsulate the internal details of a "Car" class, such as engine specifications and manufacturing processes, to protect sensitive information and provide a controlled interface for interacting with the car.
+
+Scenario 2: Health Tracking Application
+In a health tracking application, encapsulate the "User" class's personal health data, such as weight, heart rate, and sleep patterns, to maintain privacy and provide controlled access to the user's health information.
+
+Scenario 3: Online Banking Application
+In an online banking application, encapsulate the "Account" class's balance and transaction history to protect the customer's financial information and ensure that interactions with the account are performed through controlled methods.
+
+Scenario 4: Smart Home Automation System
+In a smart home automation system, encapsulate the internal workings of devices like "Thermostat" and "LightingController" to provide a simplified interface for controlling and monitoring these devices while hiding complex implementation details.
+
+Scenario 5: Customer Relationship Management (CRM) System
+In a CRM system, encapsulate the customer data and methods for managing customer interactions to ensure data integrity, privacy, and controlled access to customer information.
+*/
