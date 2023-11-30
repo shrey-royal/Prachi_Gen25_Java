894297babf139bdb664d7440dceb627cb7b0aaac
Shrey K <srkkadia@gmail.com>
2023-11-30 11:59:42 +0530

    String Methods

---
+++ Strings/Stringss.java
@@ -0,0 +1,42 @@
+public class Stringss {
+    public static void main(String[] args) {
+        String str = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum nemo alias consequuntur dolorum odit aperiam in sed, autem culpa accusantium voluptates, facilis cupiditate quia natus. Possimus, maxime quod cum esse natus aliquam. Ab placeat reiciendis, veritatis blanditiis itaque eos quibusdam nam ipsa nemo dicta temporibus velit minima voluptates dignissimos beatae.";
+
+        // System.out.println("str.charAt(3): " + str.charAt(3));
+        // System.out.println("str.length(): " + str.length());
+        // System.out.println("str.substring(3): " + str.substring(360));
+        // System.out.println("str.substring(3, 10): " + str.substring(7, 13));
+        // System.out.println("str.indexOf(\"voluptates\"): " + str.indexOf("voluptates"));
+        // System.out.println("str.indexOf(\"voluptates\"): " + str.indexOf("voluptates", 140));
+        // System.out.println("str.lastIndexOf(\"voluptates\"): " + str.lastIndexOf("voluptates", 200));
+        // System.out.println("str.startsWith(\"Lorem\"): " + str.startsWith("Lorem"));
+        // System.out.println("str.endsWith(\"beatae.\"): " + str.endsWith("beatae."));
+        // System.out.println("\"Prachi Patel\".toUpperCase(): " + "Prachi Patel".toUpperCase());
+        // System.out.println("\"Prachi Patel\".toLowerCase(): " + "Prachi Patel".toLowerCase());
+        // System.out.println("\"    a b c d    \".trim(): " + "    a b c d    ".trim());
+        // System.out.println("\"Prachi Patel\".replace(\"Patel\", \"Mishra\"): " + "Prachi Patel".replace("Patel", "Mishra"));
+        // System.out.println("\"Prachi Patel\".replace(\"a\", \"A\"): " + "Prachi Patel".replace("a", "A"));
+        // System.out.println("\"Prachi Patel is learning Java.\".contains(\"Java\"): " + "Prachi Patel is learning Java.".contains("Java"));
+        // System.out.println("\"\".isEmpty(): " + "".isEmpty());
+
+        // char ch[] = "Prachi Patel".toCharArray();
+        // for (int i = 0; i < ch.length; i++) {
+        //     System.out.println(ch[i]);
+        // }
+
+        // String str1 = "1234";
+        // System.out.println(str1.getClass());
+        // int num = Integer.parseInt(str1);
+        // System.out.println(((Object) num).getClass());
+        // String str2 = String.valueOf(num);
+        // System.out.println(str2.getClass());
+
+        // System.out.println("Prachi".compareTo("prachi"));
+        // System.out.println("Prachi".compareToIgnoreCase("prachi"));
+        // System.out.println("Prachi".equals("prachi"));
+        // System.out.println("Prachi".equalsIgnoreCase("prachi"));
+        // System.out.println("Prachi".concat(" Patel"));
+        // System.out.println(String.format("My name is %s and I am %d years old.", "Prachi", 78));
+        // System.out.println(String.join(" ", "Prachi", "Patel", "is", "learning", "Java."));
+    }
+}
